MoKee/android_frameworks_base

fwb: Port extended screenshot function from OOS (cleaned)
Change-Id: I2db7d343a7f2a9647ac1e0d8c9c1349be190959c

packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -674,7 +674,11 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    mScreenshotHelper.takeScreenshot(1, true, true, mHandler);
+                    try {
+                        WindowManagerGlobal.getWindowManagerService().takeOPScreenshot(1, 0);
+                    } catch (RemoteException e) {
+                        Log.e(TAG, "Error while trying to takeOPScreenshot.", e);
+                    }
                     MetricsLogger.action(mContext,
                             MetricsEvent.ACTION_SCREENSHOT_POWER_MENU);
                 }

jmjxperia/android_frameworks_base-1

 Conflicts:
	services/core/java/com/android/server/policy/PhoneWindowManager.java

packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -940,7 +940,11 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    mScreenshotHelper.takeScreenshot(1, true, true, mHandler);
+                    try {
+                        WindowManagerGlobal.getWindowManagerService().takeScreenshot(1);
+                    } catch (RemoteException e) {
+                        Log.e(TAG, "Error while trying to takeScreenshot.", e);
+                    }
                     MetricsLogger.action(mContext,
                             MetricsEvent.ACTION_SCREENSHOT_POWER_MENU);
                 }